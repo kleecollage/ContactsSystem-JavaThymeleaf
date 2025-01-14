11fdb0c0205afbeabeeed1ef2f912c8654022310
Klee Collage <klee.collage@gmail.com>
2025-01-13 21:06:28 -0600

    feat(controller, edit.html): Implement editContact method, Create edit template

--- src/main/java/gm/contacts/controller/ContactController.java
+++ src/main/java/gm/contacts/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    // GET [host]
     @GetMapping("/")
     public String init(ModelMap model) {
         List<Contact> contacts = contactService.listContacts();
@@ -30,17 +32,37 @@ public class ContactController {
         return "index";
     }
 
+    // GET [host/add]
     @GetMapping("/add")
     public String showAdd(ModelMap model) {
-        return "add";
+        return "add"; // to add page
     }
 
+    // POST [host/add]
     @PostMapping("/add")
     public String addContact(@ModelAttribute("contactForm") Contact contact) {
         logger.info("contact to add: {}", contact);
         contactService.saveContact(contact);
         return "redirect:/"; // to Home
     }
+
+    // GET [host/edit/{id}]
+    @GetMapping("/edit/{id}")
+    public String showEdit(@PathVariable(value = "id") int idContact, ModelMap model) {
+        Contact contact = contactService.searchContact(idContact);
+        logger.info("contact to edit: {}", contact);
+        model.put("contact", contact);
+        return "edit"; // to edit page
+    }
+
+    // POST [host/edit]
+    @PostMapping("/edit")
+    public String editContact(@ModelAttribute("contact") Contact contact) {
+        logger.info("contact to update: {}", contact);
+        contactService.saveContact(contact);
+        return "redirect:/"; // to Home
+    }
+
 }
 
 
