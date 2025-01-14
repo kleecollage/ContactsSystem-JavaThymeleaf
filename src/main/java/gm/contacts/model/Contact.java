c1ad36bc64cc8c3836ecdfce5520ee489f9d26b3
Klee Collage <klee.collage@gmail.com>
2025-01-13 20:17:43 -0600

    fix(model): Implement getters and setters manually (lombok annotations dont work)

--- src/main/java/gm/contacts/model/Contact.java
+++ src/main/java/gm/contacts/model/Contact.java
@@ -22,6 +22,38 @@ public class Contact {
     String phone;
     String email;
 
+    public Integer getIdContact() {
+        return idContact;
+    }
+
+    public void setIdContact(Integer idContact) {
+        this.idContact = idContact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Contact{" +
