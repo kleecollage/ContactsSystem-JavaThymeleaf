57f4fd7fd478fe5894cc453d9dccfab2a1db8e81
Klee Collage <klee.collage@gmail.com>
2025-01-13 19:37:02 -0600

    feat(model): Create Contact Model with JPA annotations

---
+++ src/main/java/gm/contacts/model/Contact.java
@@ -0,0 +1,24 @@
+package gm.contacts.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idContact;
+    String name;
+    String phone;
+    String email;
+}
